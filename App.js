c8d7e536ac29e3677b1a1d5ccdc1dc7b5000182e
beyza70 <106872792+beyza70@users.noreply.github.com>
2022-08-26 15:51:03 +0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,36 +1,53 @@
-
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
-import './App.css';
-import Login from './Login';
-import Register from './Register';
-import YeniAnasayfa from "./YeniAnasayfa";
-
-
+import React, { useState } from "react";
+import "./App.css";
+import Axios from 'axios';
 
 function App() {
+
+  const [usernameReg, setUsernameReg] =useState("");
+  const [passwordReg, setPasswordReg] =useState("");
+
+  const register = () => {
+    Axios.post("https://localhost3001/register",{
+      username: usernameReg,
+      password: passwordReg,
+    }).then((response) => {
+      console.log(response);
+    });
+  };
+
+
   return (
     <div className="App">
-      <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="register" element={<Register />} />
-      <Route path="yenianasayfa" element={<YeniAnasayfa />} />
-      
-    </Routes>
-  </BrowserRouter>
-      
+      <div className="registration">
+        <h1>Kayıt Ol</h1>
+        <label>Username</label>
+        <input
+        type="text"
+        onChange={(e) => {
+          setUsernameReg(e.target.value);
+        }}
+        />
+        <label>Password</label>
+        <input type= "text"
+        onChange={(e) => {
+          setPasswordReg(e.target.value);
+        }}/>
+        <button>Kaydet</button>
+      </div>
+      <div className="login">
+        <h1>Login</h1>
+        <input type= "text" placeholder="Username..."/>
+        <input type= "password" placeholder="Password..."/>
+        <button>Giriş</button>
+
+
+      </div>
+
+
+
     </div>
   );
 }
 
 export default App;
-
-/* <Register/>
-<br></br>
-<Login/>
-<br></br>
-<anasayfa/> */
