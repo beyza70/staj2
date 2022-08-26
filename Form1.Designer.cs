80713fa4de4be2c8f590128ddbfd02aa952fed8f
beyza70 <106872792+beyza70@users.noreply.github.com>
2022-08-26 15:59:56 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace ip
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox_adi = new System.Windows.Forms.TextBox();
+            this.textBox_giris = new System.Windows.Forms.TextBox();
+            this.textBox_ip = new System.Windows.Forms.TextBox();
+            this.lbl_ip = new System.Windows.Forms.Label();
+            this.lbl_giris = new System.Windows.Forms.Label();
+            this.lbl_bilg_adi = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox_mac = new System.Windows.Forms.TextBox();
+            this.lbl_mac = new System.Windows.Forms.Label();
+            this.lbl_islemci = new System.Windows.Forms.Label();
+            this.lbl_ram = new System.Windows.Forms.Label();
+            this.textBox_islemci = new System.Windows.Forms.TextBox();
+            this.textBox_ram = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox_adi
+            // 
+            this.textBox_adi.Location = new System.Drawing.Point(133, 37);
+            this.textBox_adi.Name = "textBox_adi";
+            this.textBox_adi.Size = new System.Drawing.Size(577, 22);
+            this.textBox_adi.TabIndex = 0;
+            // 
+            // textBox_giris
+            // 
+            this.textBox_giris.Location = new System.Drawing.Point(133, 102);
+            this.textBox_giris.Name = "textBox_giris";
+            this.textBox_giris.Size = new System.Drawing.Size(577, 22);
+            this.textBox_giris.TabIndex = 1;
+            // 
+            // textBox_ip
+            // 
+            this.textBox_ip.Location = new System.Drawing.Point(133, 68);
+            this.textBox_ip.Name = "textBox_ip";
+            this.textBox_ip.Size = new System.Drawing.Size(577, 22);
+            this.textBox_ip.TabIndex = 2;
+            // 
+            // lbl_ip
+            // 
+            this.lbl_ip.AutoSize = true;
+            this.lbl_ip.Location = new System.Drawing.Point(23, 74);
+            this.lbl_ip.Name = "lbl_ip";
+            this.lbl_ip.Size = new System.Drawing.Size(64, 16);
+            this.lbl_ip.TabIndex = 3;
+            this.lbl_ip.Text = "IP Adresi:";
+            // 
+            // lbl_giris
+            // 
+            this.lbl_giris.AutoSize = true;
+            this.lbl_giris.Location = new System.Drawing.Point(23, 108);
+            this.lbl_giris.Name = "lbl_giris";
+            this.lbl_giris.Size = new System.Drawing.Size(71, 16);
+            this.lbl_giris.TabIndex = 4;
+            this.lbl_giris.Text = "Giriş Saati:";
+            // 
+            // lbl_bilg_adi
+            // 
+            this.lbl_bilg_adi.AutoSize = true;
+            this.lbl_bilg_adi.Location = new System.Drawing.Point(23, 43);
+            this.lbl_bilg_adi.Name = "lbl_bilg_adi";
+            this.lbl_bilg_adi.Size = new System.Drawing.Size(93, 16);
+            this.lbl_bilg_adi.TabIndex = 7;
+            this.lbl_bilg_adi.Text = "Bilgisayar Adı:";
+            // 
+            // textBox_mac
+            // 
+            this.textBox_mac.Location = new System.Drawing.Point(133, 135);
+            this.textBox_mac.Name = "textBox_mac";
+            this.textBox_mac.Size = new System.Drawing.Size(577, 22);
+            this.textBox_mac.TabIndex = 8;
+            // 
+            // lbl_mac
+            // 
+            this.lbl_mac.AutoSize = true;
+            this.lbl_mac.Location = new System.Drawing.Point(23, 141);
+            this.lbl_mac.Name = "lbl_mac";
+            this.lbl_mac.Size = new System.Drawing.Size(78, 16);
+            this.lbl_mac.TabIndex = 9;
+            this.lbl_mac.Text = "Mac Adresi:";
+            // 
+            // lbl_islemci
+            // 
+            this.lbl_islemci.AutoSize = true;
+            this.lbl_islemci.Location = new System.Drawing.Point(23, 176);
+            this.lbl_islemci.Name = "lbl_islemci";
+            this.lbl_islemci.Size = new System.Drawing.Size(49, 16);
+            this.lbl_islemci.TabIndex = 10;
+            this.lbl_islemci.Text = "İşlemci";
+            // 
+            // lbl_ram
+            // 
+            this.lbl_ram.AutoSize = true;
+            this.lbl_ram.Location = new System.Drawing.Point(23, 208);
+            this.lbl_ram.Name = "lbl_ram";
+            this.lbl_ram.Size = new System.Drawing.Size(36, 16);
+            this.lbl_ram.TabIndex = 11;
+            this.lbl_ram.Text = "Ram";
+            // 
+            // textBox_islemci
+            // 
+            this.textBox_islemci.Location = new System.Drawing.Point(133, 170);
+            this.textBox_islemci.Name = "textBox_islemci";
+            this.textBox_islemci.Size = new System.Drawing.Size(577, 22);
+            this.textBox_islemci.TabIndex = 12;
+            // 
+            // textBox_ram
+            // 
+            this.textBox_ram.Location = new System.Drawing.Point(133, 204);
+            this.textBox_ram.Name = "textBox_ram";
+            this.textBox_ram.Size = new System.Drawing.Size(577, 22);
+            this.textBox_ram.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox_ram);
+            this.Controls.Add(this.textBox_islemci);
+            this.Controls.Add(this.lbl_ram);
+            this.Controls.Add(this.lbl_islemci);
+            this.Controls.Add(this.lbl_mac);
+            this.Controls.Add(this.textBox_mac);
+            this.Controls.Add(this.lbl_bilg_adi);
+            this.Controls.Add(this.lbl_giris);
+            this.Controls.Add(this.lbl_ip);
+            this.Controls.Add(this.textBox_ip);
+            this.Controls.Add(this.textBox_giris);
+            this.Controls.Add(this.textBox_adi);
+            this.Name = "Form1";
+            this.Text = "Ana Sayfa";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_adi;
+        private System.Windows.Forms.TextBox textBox_giris;
+        private System.Windows.Forms.TextBox textBox_ip;
+        private System.Windows.Forms.Label lbl_ip;
+        private System.Windows.Forms.Label lbl_giris;
+        private System.Windows.Forms.Label lbl_bilg_adi;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox_mac;
+        private System.Windows.Forms.Label lbl_mac;
+        private System.Windows.Forms.Label lbl_islemci;
+        private System.Windows.Forms.Label lbl_ram;
+        private System.Windows.Forms.TextBox textBox_islemci;
+        private System.Windows.Forms.TextBox textBox_ram;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Data.SqlClient;
+using System.Net.NetworkInformation;
+using System.Management;
+
+using System.Diagnostics;
+using System.Deployment.Application;
+using System.Collections;
+
+using System.IO;
+
+using System.Reflection;
+using Microsoft.Win32;
+
+
+namespace ip
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            string mac = "";
+            foreach (NetworkInterface nic in NetworkInterface.GetAllNetworkInterfaces())
+            {
+
+                if (nic.OperationalStatus == OperationalStatus.Up && (!nic.Description.Contains("Virtual") && !nic.Description.Contains("Pseudo")))
+                {
+                    if (nic.GetPhysicalAddress().ToString() != "")
+                    {
+                        mac = nic.GetPhysicalAddress().ToString();
+                    }
+                }
+            }
+
+            string ram = "";
+            ManagementObjectSearcher Search = new ManagementObjectSearcher("Select * From Win32_ComputerSystem");
+
+            foreach (ManagementObject Mobject in Search.Get())
+            {
+                double Ram_Bytes = (Convert.ToDouble(Mobject["TotalPhysicalMemory"]));
+                double ramgb = Ram_Bytes / 1073741824;
+                double islem = Math.Ceiling(ramgb);
+                ram = islem.ToString() + " GB";
+            }
+
+
+            string islemci = string.Empty;
+            ManagementObjectSearcher mos =new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_Processor");
+            
+            foreach (ManagementObject mo in mos.Get())
+            {
+                islemci = (string)mo["Name"];
+            }
+
+
+            string bilgisayarAdi = Dns.GetHostName();
+            textBox_adi.Text = bilgisayarAdi;
+            string ipAdresi = Dns.GetHostByName(bilgisayarAdi).AddressList[0].ToString();
+            textBox_ip.Text = ipAdresi;
+            textBox_mac.Text = mac;
+            textBox_ram.Text = ram;
+            textBox_islemci.Text = islemci;
+            timer1.Start();
+            textBox_giris.Text = DateTime.Now.ToString();
+
+
+
+            SqlConnection baglanti = new SqlConnection("Data Source=BEYZA;Initial Catalog=ip_adresi;Integrated Security=True");
+            
+            if (baglanti.State == ConnectionState.Closed)
+                baglanti.Open();
+                
+            string kayit = "insert into bilgiler4(bilgisayar_adi,ip_adresi,giris_saati,mac_adresi,islemci,ram) values (@adi,@ip,@giris,@mac,@islemci,@ram)";
+            SqlCommand komut = new SqlCommand(kayit, baglanti);
+               
+            komut.Parameters.AddWithValue("@adi", textBox_adi.Text);
+            komut.Parameters.AddWithValue("@ip", textBox_ip.Text);
+            komut.Parameters.AddWithValue("@giris", textBox_giris.Text);
+            komut.Parameters.AddWithValue("@mac", textBox_mac.Text);
+            komut.Parameters.AddWithValue("@islemci", textBox_islemci.Text);
+            komut.Parameters.AddWithValue("@ram", textBox_ram.Text);
+
+
+            komut.ExecuteNonQuery();
+                
+            baglanti.Close();
+                
+            //Environment.Exit(0);
+
+            //TextBox lardaki bilgilerin veritabanında daha önce kayıt edilmiş mi kontrol edilmesi kaldı
+
+
+
+
+        }
+
+
+    }
+}
